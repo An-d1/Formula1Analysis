f02b2bbd558695399446950c30aadbbe59d6b92e
An-d1 <andisbara@gmail.com>
2025-11-01 02:16:15 +0100

    Constructed folder logic, createad a fetchData script which stores the cache at the specified folder, and also loads the session from the race, which takes 3 parameters and so can dynamically changed based on what race needs to be analysed

---
+++ scripts/fetch_data.py
@@ -0,0 +1,27 @@
+import fastf1
+from pathlib import Path 
+
+def setup_fastf1_cache():
+   # Gets path to this file
+    script_path = Path(__file__).resolve()
+    
+    # gets the project root (parent of scripts)
+    project_root = script_path.parent.parent
+    
+    # define cache path from the root
+    cache_dir = project_root / "external_data" / "fastf1"
+    
+    # enable the cache to store the data locally
+    fastf1.Cache.enable_cache(cache_dir)
+
+def load_session(year: int, gp: str, session_type: str = "R"): #session type currently set to race so it only accepts races, maybe future method will be added to accept quali and etc
+    #dynamically fetch and load an f1 session from fastf1
+
+    setup_fastf1_cache()
+    session = fastf1.get_session(year, gp, session_type)
+    session.load()
+    return session
+
+
+
+    
